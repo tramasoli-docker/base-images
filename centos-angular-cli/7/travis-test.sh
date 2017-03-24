6de75653b7ede9e2d38dfa12db015e9f1e8a0a69
Fábio Tramasoli <tramasoli@tj.rs.gov.br>
2017-03-24 16:36:35 -0300

    set -x no script, travis não mostra o output

--- centos-angular-cli/7/travis-test.sh
+++ centos-angular-cli/7/travis-test.sh
@@ -1,3 +1,4 @@
+set -x
 docker exec -it $1 java -version | grep 1.8 || exit 1
 docker exec -it $1 --version | grep v6. || exit 1
 docker exec -it $1 ng version | grep angular\/cli\:\ 1.0.0-rc\.4 || exit 1
