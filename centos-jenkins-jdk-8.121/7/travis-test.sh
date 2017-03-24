3473db64bab849ab019a65da66b84111acc875f9
Fábio Tramasoli <tramasoli@tj.rs.gov.br>
2017-03-24 17:04:56 -0300

    Correção do teste SSH

--- centos-jenkins-jdk-8.121/7/travis-test.sh
+++ centos-jenkins-jdk-8.121/7/travis-test.sh
@@ -1,5 +1,5 @@
 set -x
-docker exec -it $1 ps aux | grep sshd || exit 
+docker exec -it $1 ps aux | grep sshd || exit 1
 docker exec -it $1 java -version | grep 1.8 || exit 1
 docker exec -it $1 mvn -version  |  grep Apache\ Maven\ 3\. || exit 1
 docker exec -it $1 svn --version || exit 1
