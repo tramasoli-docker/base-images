7729af949756940d24eab2608561ede2c7a8024d
Fábio Tramasoli <tramasoli@tj.rs.gov.br>
2017-03-24 17:42:43 -0300

    Correção do teste de oraclelinux-jenkins-jdk-8.121

--- oraclelinux-jenkins-jdk-7.80/7/travis-test.sh
+++ oraclelinux-jenkins-jdk-7.80/7/travis-test.sh
@@ -1,4 +1,4 @@
-set -x
+se  x
 #docker exec -it $1 ps aux | grep sshd || exit 1
 docker exec -it $1 java -version | grep 1.7 || exit 1
 docker exec -it $1 mvn -version  |  grep Apache\ Maven\ 3\. || exit 1
--- oraclelinux-jenkins-jdk-8.121/7/travis-test.sh
+++ oraclelinux-jenkins-jdk-8.121/7/travis-test.sh
@@ -1,6 +1,7 @@
 set -x
 #docker exec -it $1 ps aux | grep sshd || exit 1
 docker exec -it $1 java -version | grep 1.8 || exit 1
-docker exec -it $1 node --version | grep v6. || exit 1
-docker exec -it $1 ng version | grep angular\/cli\:\ 1.0.0-rc\.4 || exit 1
+docker exec -it $1 mvn -version  |  grep Apache\ Maven\ 3\. || exit 1
+docker exec -it $1 svn --version || exit 1
+docker exec -it $1 git --version  || exit 1
 exit 0
