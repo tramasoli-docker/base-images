d3e766e5bbb88dd2c760fc46aa7e63701d759f0b
Carlos Manoel <carlos.o@tjrs.jus.br>
2017-05-02 17:41:54 -0300

    adciona jq (command-line JSON processor)

--- centos-angular-cli/7/travis-test.sh
+++ centos-angular-cli/7/travis-test.sh
@@ -3,4 +3,5 @@ set -x
 docker exec -it $1 java -version | grep 1.8 || exit 1
 docker exec -it $1 node --version | grep v6. || exit 1
 docker exec -it $1 ng version | grep angular\/cli\:\ 1.0.0-rc\.4 || exit 1
+docker exec -it $1 jq --version | grep 1.5 || exit 1
 exit 0
