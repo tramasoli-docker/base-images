0b498c97a2325b1bb791e2ea70e48089a9a29043
Fábio Tramasoli <fabio@tramasoli.com>
2017-06-06 10:27:49 -0300

    Closes #1 , Closes #2

--- oraclelinux-jenkins-jdk-7.80/7/travis-test.sh
+++
@@ -1,7 +0,0 @@
-se  x
-#docker exec -it $1 ps aux | grep sshd || exit 1
-docker exec -it $1 java -version | grep 1.7 || exit 1
-docker exec -it $1 mvn -version  |  grep Apache\ Maven\ 3\. || exit 1
-docker exec -it $1 svn --version || exit 1
-docker exec -it $1 git --version  || exit 1
-exit 0
